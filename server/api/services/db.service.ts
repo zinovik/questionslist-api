7ef94a9b82436db13edd9a2319c7aa6bd24732ed
Max Zinovik <zinovik@gmail.com>
2018-09-23 17:49:52 +0300

    Updated responses structure

--- server/api/services/db.service.ts
+++ server/api/services/db.service.ts
@@ -7,18 +7,31 @@ client.connect();
 client.query('CREATE TABLE categories(' +
   'id SERIAL PRIMARY KEY, ' +
   'name VARCHAR(40) not null, ' +
+  'created VARCHAR(70) not null, ' +
+  'modified VARCHAR(70) not null, ' +
   'parent INT, ' +
   'FOREIGN KEY (parent) REFERENCES categories(id))', (err, res) => {
-    console.log(err && err.message || res);
+    if (err) {
+      L.info(err.message);
+    } else {
+      L.info(res);
+    }
     client.query('CREATE TABLE questions(' +
       'id SERIAL PRIMARY KEY, ' +
       'name VARCHAR(40) not null, ' +
       'text VARCHAR(300) not null, ' +
-      'difficulty INT, ' +
+      'difficulty INT not null, ' +
       'answer VARCHAR(200) not null, ' +
+      'tags VARCHAR(200), ' +
+      'created VARCHAR(70) not null, ' +
+      'modified VARCHAR(70) not null, ' +
       'category INT, ' +
       'FOREIGN KEY (category) REFERENCES categories(id))', (err, res) => {
-        console.log(err && err.message || res);
+        if (err) {
+          L.info(err.message);
+        } else {
+          L.info(res);
+        }
       });
   });
 
@@ -29,40 +42,52 @@ interface Question {
   difficulty: number,
   answer: string,
   category: number,
+  tags: string,
+  created: string,
+  modified: string,
 };
 
 interface Category {
   id: number,
   name: string,
   parent: string,
+  created: string,
+  modified: string,
 };
 
 export class DbService {
-  getAllQuestions(): Promise<Question[]> {
+  getAllQuestions(): Promise<{ error: string, data: Question[] }> {
     return new Promise((resolve, reject) => {
+      L.info('fetch all questions');
       client.query('SELECT * FROM questions ORDER BY id ASC;', (err, res) => {
-        L.info(res.rows, 'fetch all questions');
-        console.log(err && err.message || (res && res.rows));
-        return resolve(res.rows);
+        if (err) {
+          L.error(err.message);
+        } else {
+          L.info(res && res.rows);
+        }
+        return resolve({ error: err && err.message, data: res.rows });
       });
     });
   }
 
-  createQuestion(question: Question): Promise<Question[]> {
+  createQuestion(question: Question): Promise<{ error: string, data: Question[] }> {
     return new Promise((resolve, reject) => {
       L.info(`create question`, question);
-      client.query(`INSERT INTO questions(name, text, difficulty, answer, category) ` +
-        `values('${question.name}', '${question.text}', '${question.difficulty}', '${question.answer}', '${question.category}')`, (err, res) => {
-          console.log(err && err.message || (res && res.rows));
+      client.query(`INSERT INTO questions(name, text, difficulty, answer, category, tags, created, modified) ` +
+        `values('${question.name}', '${question.text}', '${question.difficulty}', '${question.answer}', ` +
+        `'${question.category}', '${question.tags}', 'anonymous', 'anonymous')`, (err, res) => {
+          if (err) {
+            L.error(err.message);
+          }
           return this.getAllQuestions()
-            .then((rows: Question[]) => {
-              resolve(rows);
+            .then(({ error, data }) => {
+              resolve({ error: err && err.message || error, data: data });
             });
         });
     });
   }
 
-  updateQuestion(question: Question): Promise<Question[]> {
+  updateQuestion(question: Question): Promise<{ error: string, data: Question[] }> {
     return new Promise((resolve, reject) => {
       L.info(`update question`, question);
       client.query(`UPDATE questions ` +
@@ -70,81 +95,96 @@ export class DbService {
         `text = '${question.text}', ` +
         `difficulty = '${question.difficulty}', ` +
         `answer = '${question.answer}', ` +
+        `tags = '${question.tags}', ` +
         `category = '${question.category}' ` +
         `WHERE id = '${question.id}'`, (err, res) => {
-          console.log(err && err.message || (res && res.rows));
+          if (err) {
+            L.error(err.message);
+          }
           return this.getAllQuestions()
-            .then((rows: Question[]) => {
-              resolve(rows);
+            .then(({ error, data }) => {
+              resolve({ error: err && err.message || error, data: data });
             });
         });
     });
   }
 
-  
-  deleteQuestion(id): Promise<Question[]> {
+
+  deleteQuestion(id): Promise<{ error: string, data: Question[] }> {
     return new Promise((resolve, reject) => {
       L.info(`delete question`, id);
       client.query(`DELETE FROM questions ` +
         `WHERE id = '${id}'`, (err, res) => {
-          console.log(err && err.message || (res && res.rows));
+          if (err) {
+            L.error(err.message);
+          }
           return this.getAllQuestions()
-            .then((rows: Question[]) => {
-              resolve(rows);
+            .then(({ error, data }) => {
+              resolve({ error: err && err.message || error, data: data });
             });
         });
     });
   }
 
-  getAllCategories(): Promise<Category[]> {
+  getAllCategories(): Promise<{ error: string, data: Category[] }> {
     return new Promise((resolve, reject) => {
+      L.info('fetch all categories');
       client.query('SELECT * FROM categories ORDER BY id ASC;', (err, res) => {
-        L.info(res.rows, 'fetch all categories');
-        console.log(err && err.message || (res && res.rows));
-        return resolve(res.rows);
+        if (err) {
+          L.error(err.message);
+        } else {
+          L.info(res && res.rows);
+        }
+        return resolve({ error: err && err.message, data: res.rows });
       });
     });
   }
 
-  createCategory(category: Category): Promise<Category[]> {
+  createCategory(category: Category): Promise<{ error: string, data: Category[] }> {
     return new Promise((resolve, reject) => {
       L.info(`create category`, category);
-      client.query(`INSERT INTO categories(name, parent) ` +
-        `values('${category.name}', '${category.parent}')`, (err, res) => {
-          console.log(err && err.message || (res && res.rows));
+      client.query(`INSERT INTO categories(name, parent, created, modified) ` +
+        `values('${category.name}', '${category.parent}', 'anonymous', 'anonymous')`, (err, res) => {
+          if (err) {
+            L.error(err.message);
+          }
           return this.getAllCategories()
-            .then((rows: Category[]) => {
-              resolve(rows);
+            .then(({ error, data }) => {
+              resolve({ error: err && err.message || error, data: data });
             });
         });
     });
   }
 
-  updateCategory(category: Category): Promise<Category[]> {
+  updateCategory(category: Category): Promise<{ error: string, data: Category[] }> {
     return new Promise((resolve, reject) => {
       L.info(`update category`, category);
       client.query(`UPDATE categories ` +
         `SET name = '${category.name}', ` +
         `parent = '${category.parent}' ` +
         `WHERE id = '${category.id}'`, (err, res) => {
-          console.log(err && err.message || (res && res.rows));
+          if (err) {
+            L.error(err.message);
+          }
           return this.getAllCategories()
-            .then((rows: Category[]) => {
-              resolve(rows);
+            .then(({ error, data }) => {
+              resolve({ error: err && err.message || error, data: data });
             });
         });
     });
   }
 
-  deleteCategory(id): Promise<Category[]> {
+  deleteCategory(id): Promise<{ error: string, data: Category[] }> {
     return new Promise((resolve, reject) => {
       L.info(`delete category`, id);
       client.query(`DELETE FROM categories ` +
         `WHERE id = '${id}'`, (err, res) => {
-          console.log(err && err.message || (res && res.rows));
+          if (err) {
+            L.error(err.message);
+          }
           return this.getAllCategories()
-            .then((rows: Category[]) => {
-              resolve(rows);
+            .then(({ error, data }) => {
+              resolve({ error: err && err.message || error, data: data });
             });
         });
     });
